1d3258bf45630c18cadd768c4b3046f13b3c8263
karol <krzotki@gmail.com>
2021-04-27 19:58:51 +0200

    added description

--- src/index.js
+++ src/index.js
@@ -14,15 +14,15 @@ class Sketch extends React.Component {
         this.state = {
             history: [{
                 selectedCircles: [
-                    
+
                 ],
 
                 circles: [
-                    
+
                 ],
 
                 lines: [
-                    
+
                 ],
 
                 weights: [
@@ -49,23 +49,23 @@ class Sketch extends React.Component {
         let inputsToRender = [];
         let pathLinesToRender = [];
 
-        for(let i = 0; i < circles.length; i++) {
+        for (let i = 0; i < circles.length; i++) {
             let selected = current.selectedCircles.includes(i);
             circlesToRender.push(this.renderCircle(i, circles[i], selected));
         }
 
-        for(let j = 0; j < lines.length; j++) {
+        for (let j = 0; j < lines.length; j++) {
             let from = lines[j].from;
             let to = lines[j].to;
 
-            if(from === to) continue;
+            if (from === to) continue;
             linesToRender.push(
-                <LineTo 
-                    borderColor = 'green' 
-                    zIndex = {0} 
-                    key = {'line' + from + "-" + to} 
-                    from = {"circle " + from}
-                    to = {"circle " + to}>
+                <LineTo
+                    borderColor='green'
+                    zIndex={0}
+                    key={'line' + from + "-" + to}
+                    from={"circle " + from}
+                    to={"circle " + to}>
                 </LineTo>
             );
 
@@ -75,46 +75,54 @@ class Sketch extends React.Component {
             };
 
             inputsToRender.push(
-                <WeightInput 
-                    key = {'weight'+ from + "-" + to} 
-                    from = {from}
-                    to = {to}
-                    position = {inputPosition} 
-                    value = {current.weights[from][to]}
-                    onClick = {(evt) => this.handleWeightInputClick(evt)}
-                    onChange = {(evt) => this.handleWeightInputChange(evt, from, to)}
-                    >
+                <WeightInput
+                    key={'weight' + from + "-" + to}
+                    from={from}
+                    to={to}
+                    position={inputPosition}
+                    value={current.weights[from][to]}
+                    onClick={(evt) => this.handleWeightInputClick(evt)}
+                    onChange={(evt) => this.handleWeightInputChange(evt, from, to)}
+                >
                 </WeightInput>
             );
         }
 
-        for(let j = 0; j < path.length; j++) {
+        for (let j = 0; j < path.length; j++) {
             let from = path[j].from;
             let to = path[j].to;
 
-            if(from === to) continue;
+            if (from === to) continue;
             pathLinesToRender.push(
-                <LineTo 
-                    borderColor = 'purple'
-                    borderWidth = {3} 
-                    zIndex = {0} 
-                    key = {'line' + from + "-" + to} 
-                    from = {"circle " + from}
-                    to = {"circle " + to}>
+                <LineTo
+                    borderColor='purple'
+                    borderWidth={3}
+                    zIndex={0}
+                    key={'line' + from + "-" + to}
+                    from={"circle " + from}
+                    to={"circle " + to}>
                 </LineTo>
             );
         }
 
-        return(
-            <div id = 'container'>
-                <div id = 'canvas' onClick = {(evt)=> this.handleClick(evt)} onContextMenu = {(evt) => this.handleRightClick(evt)}>
+        return (
+            <div id='container'>
+                <div id='canvas' onClick={(evt) => this.handleClick(evt)} onContextMenu={(evt) => this.handleRightClick(evt)}>
                     {linesToRender}
                     {circlesToRender}
                     {inputsToRender}
                     {pathLinesToRender}
-               </div>
-               <Matrix weights = {current.weights}></Matrix>
-               <button className = 'solve' onClick = {(evt) => this.findShortestPath(evt, current.weights)}>Find shortest path</button>
+                </div>
+                <Matrix weights={current.weights}></Matrix>
+                
+                <section className='description'>
+                <button className='solve' onClick={(evt) => this.findShortestPath(evt, current.weights)}>Find shortest path</button>
+                    <p><b>Left click</b> - add node</p>
+                    <p><b>Right click</b> - revert change</p>
+                    <p>Choose two nodes to create link</p>
+                    <p>For pathfinding* Dijkstra's alghorithm was used</p>
+                    <p>* path between point 0 and last</p>
+                </section>
             </div>
         );
     }
@@ -126,7 +134,7 @@ class Sketch extends React.Component {
         const current = history[history.length - 1];
         const path = current.path.slice();
 
-        while(path.length > 0) path.pop();
+        while (path.length > 0) path.pop();
 
         let index = weights.length - 1;
 
@@ -138,9 +146,9 @@ class Sketch extends React.Component {
 
             index = path[path.length - 1].to;
 
-        } while(index > -1)
+        } while (index > -1)
 
-        let newHistoryItem = {...current};
+        let newHistoryItem = { ...current };
         newHistoryItem.path = path;
 
         this.setState({
@@ -156,39 +164,38 @@ class Sketch extends React.Component {
         const lines = current.lines.slice();
         const weights = current.weights.slice();
 
-        if(selectedCircles[0] !== index) {
+        if (selectedCircles[0] !== index) {
             selectedCircles.push(index);
         } else {
             selectedCircles.pop();
         }
 
-        if(selectedCircles.length === 2) {
+        if (selectedCircles.length === 2) {
             let from = selectedCircles[0];
             let to = selectedCircles[1];
 
-            if(lines.filter(line => (line.from === from && line.to === to) || (line.from === to && line.to === from)).length === 0)
-            {
+            if (lines.filter(line => (line.from === from && line.to === to) || (line.from === to && line.to === from)).length === 0) {
                 lines.push({
                     from: from,
                     to: to,
                 });
-    
-                const fromRow = weights[from].slice();    
+
+                const fromRow = weights[from].slice();
                 fromRow[to] = 1;
                 weights[from] = fromRow;
-    
+
                 const toRow = weights[to].slice();
                 toRow[from] = 1;
                 weights[to] = toRow;
             }
-               
+
             selectedCircles.pop();
             selectedCircles.pop();
 
-            
+
         }
 
-        let newHistoryItem = {...current};
+        let newHistoryItem = { ...current };
         newHistoryItem.selectedCircles = selectedCircles;
         newHistoryItem.lines = lines;
         newHistoryItem.weights = weights;
@@ -201,15 +208,15 @@ class Sketch extends React.Component {
     handleRightClick(evt) {
         evt.preventDefault();
 
-        if(this.state.history.length > 1) {
-            const history = this.state.history.slice(0,  this.state.history.length - 1);
-            this.setState({history: history});
+        if (this.state.history.length > 1) {
+            const history = this.state.history.slice(0, this.state.history.length - 1);
+            this.setState({ history: history });
         }
     }
 
     handleWeightInputClick(evt) {
         evt.stopPropagation();
-    } 
+    }
 
     handleWeightInputChange(evt, from, to) {
         const history = this.state.history;
@@ -218,7 +225,7 @@ class Sketch extends React.Component {
 
         let value = Number(evt.target.value);
 
-        const fromRow = weights[from].slice();    
+        const fromRow = weights[from].slice();
         fromRow[to] = value;
         weights[from] = fromRow;
 
@@ -226,7 +233,7 @@ class Sketch extends React.Component {
         toRow[from] = value;
         weights[to] = toRow;
 
-        let newHistoryItem = {...current};
+        let newHistoryItem = { ...current };
         newHistoryItem.weights = weights;
 
         this.setState({
@@ -248,13 +255,13 @@ class Sketch extends React.Component {
 
         weights.push(Array(circles.length).fill(0));
 
-        for(let i = 0; i < circles.length - 1; i++) {
+        for (let i = 0; i < circles.length - 1; i++) {
             let row = weights[i].slice();
-            row[circles.length - 1]  = 0;
-            weights[i] = row;       
+            row[circles.length - 1] = 0;
+            weights[i] = row;
         }
-        
-        let newHistoryItem = {...current};
+
+        let newHistoryItem = { ...current };
         newHistoryItem.circles = circles;
         newHistoryItem.weights = weights;
 
@@ -265,13 +272,13 @@ class Sketch extends React.Component {
     }
 
     renderCircle(index, circle, selected) {
-        return <Circle 
-            key = {index} 
-            index = {index} 
-            params = {circle}
-            onClick = {(evt, index) => this.handleCircleClick(evt, index)}
-            color = {(selected)? 'blue' : 'red'}
-            >
+        return <Circle
+            key={index}
+            index={index}
+            params={circle}
+            onClick={(evt, index) => this.handleCircleClick(evt, index)}
+            color={(selected) ? 'blue' : 'red'}
+        >
         </Circle>
     }
 }
@@ -279,5 +286,4 @@ class Sketch extends React.Component {
 ReactDOM.render(
     <Sketch />,
     document.getElementById('root')
-  );
-  
+);
